978e1eca3c54f6534675072f18e860023b60f6ce
Joel Bodenmann <joel.bodenmann@simulton.com>
2020-12-22 17:20:09 +0100

    Add the actual script

---
+++ send_sms_vonage.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+BIN_CURL=/usr/local/bin/curl
+API_PATH="https://rest.nexmo.com/sms/json"
+API_KEY=xxx
+API_SECRET=xxx
+SMS_TO=$1
+SMS_FROM=Zabbix
+SMS_TEXT="$2%0a---%0a$3"
+
+# Send SMS
+$BIN_CURL -X POST $API_PATH \
+        -d api_key=$API_KEY \
+        -d api_secret=$API_SECRET \
+        -d to=$SMS_TO \
+        -d from=$SMS_FROM \
+        -d text="$SMS_TEXT"
